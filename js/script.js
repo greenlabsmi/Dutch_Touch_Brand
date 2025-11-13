917cc4e7fa16e6168018f291da0d87dde3aee491
greenlabsmi <shop@greenlabsmi.com>
2025-11-13 15:21:28 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("active");
+}
+
+// HERO SLIDER
+let slideIndex = 0;
+const slides = document.querySelectorAll(".hero-slide");
+
+setInterval(() => {
+  slides[slideIndex].classList.remove("active");
+  slideIndex = (slideIndex + 1) % slides.length;
+  slides[slideIndex].classList.add("active");
+}, 4200);
 
