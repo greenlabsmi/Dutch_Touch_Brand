d62f4d2f2b601c33dbebb82583234d83f05c9054
greenlabsmi <shop@greenlabsmi.com>
2025-12-05 15:14:53 -0500

    Update strains.js

--- js/strains.js
+++ js/strains.js
@@ -381,13 +381,3 @@ cards.forEach(card => {
     }
   }
 });
-
-window.addEventListener('scroll', () => {
-  const nav = document.querySelector('.navbar');
-  if (window.scrollY > 40) {
-    nav.style.background = '#000';
-  } else {
-    nav.style.background = 'transparent';
-  }
-});
-
