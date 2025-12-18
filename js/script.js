2da7468e8bc2044eac4f8f93c616bc0a1f6377d8
greenlabsmi <shop@greenlabsmi.com>
2025-12-18 17:28:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@ function startHero() {
     slides[index].classList.remove("active");
     index = (index + 1) % slides.length;
     slides[index].classList.add("active");
-  }, 6000);
+  }, 4500);
 }
 
 function pauseHero() {
