fe7fa40b76d46d07f6465315fda16260d461656a
greenlabsmi <shop@greenlabsmi.com>
2026-04-13 11:43:12 -0400

    Update strains-loader.js

--- js/strains-loader.js
+++ js/strains-loader.js
@@ -22,10 +22,11 @@ async function loadStrainLibrary() {
         const trophyHTML = `
           <div class="strain-card trophy-card">
             <div class="strain-card-inner">
-              <div class="strain-image" style="background-image: url('${imageUrl}');"></div>
+              <div class="strain-image" style="background-image: url('${imageUrl}');">
+                <span class="corner-award-badge">AWARD WINNER</span>
+              </div>
               <div class="strain-top">
                 <h3 class="strain-name">${strain.name}</h3>
-                <span class="strain-badge">🏆 AWARD WINNER</span>
               </div>
               <p class="strain-meta">${strain.type.toUpperCase()} | Genetics by ${strain.breeder}</p>
               <div class="strain-content">
@@ -41,7 +42,7 @@ async function loadStrainLibrary() {
       // 2. MAIN VAULT LIBRARY (DESKTOP NORMAL + MOBILE 3D FLIP)
       // ==========================================
       const vaultHTML = `
-        <article class="strain-card library-card">
+        <article class="strain-card library-card ${strain.award ? 'is-award-winner' : ''}">
           <div class="strain-card-inner deli-card" onclick="if(window.innerWidth <= 768) this.classList.toggle('is-flipped');">
             
             <div class="deli-card__front" style="background-image: url('${imageUrl}');">
@@ -50,11 +51,12 @@ async function loadStrainLibrary() {
             </div>
 
             <div class="deli-card__back">
-              <div class="${imageClass} desktop-img" style="background-image: url('${imageUrl}');"></div>
+              <div class="${imageClass} desktop-img" style="background-image: url('${imageUrl}');">
+                ${strain.award ? `<span class="corner-award-badge desktop-badge">AWARD WINNER</span>` : ''}
+              </div>
               <div class="strain-content">
                 <div class="strain-top">
                   <h3 class="strain-name">${strain.name}</h3>
-                  ${strain.award ? `<span class="strain-badge desktop-badge">Award Winner</span>` : ''}
                 </div>
                 <p class="strain-meta">${strain.type.toUpperCase()} | Genetics by ${strain.breeder}</p>
                 <p class="strain-meta" style="color: #d4af37; font-size: 0.85rem;">${strain.lineage}</p>
